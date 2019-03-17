91f3be771e04f682e7dec772d19cede4d10739e0
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-03-17 00:16:58 -0400

    dev keys added

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '',
+  googleClientSecret: '',
+  mongoURI: 'mongodb+srv://constantine:foryou88@hr-notify-nc544.mongodb.net/hr-notify-dev',
+  cookieKey: '',
+};
