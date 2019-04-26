6a5b672e8e7e4f1aa3b26573eb1dcbefb8af696c
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-04-25 23:16:20 -0400

    removed dev keys

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID: '',
-  googleClientSecret: '',
-  mongoURI: 'mongodb+srv://constantine:foryou88@hr-notify-nc544.mongodb.net/hr-notify-dev',
-  cookieKey: '',
-  secret: 'sdfhlghjfa;sldfja;lsdfj;asdf',
-  frontendHost: 'http://localhost:3000',
-};
