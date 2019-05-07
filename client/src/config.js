1f581315659c54d199d5311ada00d9d7a9e37536
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-05-06 22:18:08 -0400

    test deploy3

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  process.env.NODE_ENV === 'production' ? 'https://my-app.com/api' : 'http://localhost:5000/api';
+  process.env.NODE_ENV === 'production' ? 'https://pedantic-kirch-b97958.netlify.com/api' : 'http://localhost:5000/api';
