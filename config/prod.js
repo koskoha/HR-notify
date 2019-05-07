dcbb3bf97442ba155b3f495e64ae871a37dd0280
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-05-06 21:41:31 -0400

    test deploy

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  // googleClientID: process.env.GOOGLE_CLIENT_ID,
+  // googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  frontendHost: '',
+  frontendHost: 'https://pedantic-kirch-b97958.netlify.com',
   sendGridKey: process.env.SEND_GRID_KEY,
 };
