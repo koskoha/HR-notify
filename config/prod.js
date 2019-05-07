ac937eaa3d860ebcf02d1c5dbe96482b56a68d82
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2019-05-07 00:41:10 -0400

    added secret var to prod build

--- config/prod.js
+++ config/prod.js
@@ -2,4 +2,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   frontendHost: 'https://agile-harbor-45745.herokuapp.com',
   sendGridKey: process.env.SEND_GRID_KEY,
+  secret: process.env.SECRET,
 };
